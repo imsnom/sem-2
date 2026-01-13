8ac63aa84a3e4c98099f1ef1691c4386850f7143
Ash <115019741+imsnom@users.noreply.github.com>
2026-01-13 13:46:30 -0500

    dunno

---
+++ ComponentFramework/SceneManager.h
@@ -0,0 +1,42 @@
+#ifndef SCENEMANAGER_H
+#define SCENEMANAGER_H
+
+#include <string>
+class SceneManager  {
+public:
+	
+	SceneManager();
+	~SceneManager();
+	void Run();
+	bool Initialize(std::string name_, int width_, int height_);
+	void HandleEvents();
+	
+	
+private:
+	enum class SCENE_NUMBER {
+		SCENE0g = 0, // g for graphics, p for physics
+		SCENE0p,
+		SCENE1g,
+		SCENE1p,
+		SCENE2g,
+		SCENE2p,
+		SCENE3g,
+		SCENE3p,
+		SCENE4g,
+		SCENE4p,
+		SCENE5,
+		SCENE6
+	};
+
+	class Scene* currentScene;
+	class Timer* timer;
+	class Window* window;
+
+	unsigned int fps;
+	bool isRunning;
+	bool fullScreen;
+	bool BuildNewScene(SCENE_NUMBER scene_);
+};
+
+
+#endif // SCENEMANAGER_H
